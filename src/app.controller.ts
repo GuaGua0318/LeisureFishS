df1ee87b2c2ab57502db26ae1f02cd363e795ccc
GuaGua0318 <15972660547@163.com>
2024-02-21 19:48:23 +0800

    初始化

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('aaa')
+  aaa() {
+    return 'aaa';
+  }
+
+  @Get('bbb')
+  bbb() {
+    return 'bbb';
+  }
+}
